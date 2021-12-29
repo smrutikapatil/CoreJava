4b8e811a1251b8108e37ff89331f86b51aeb0b65
smrutikapatil <smruti.patil17@gmail.com>
2021-12-29 18:54:17 +0530

    Added- identified largest number

---
+++ LargestThreeNum.java
@@ -0,0 +1,34 @@
+package bridgelabz.coreprograms;
+
+import java.util.Scanner;
+
+public class LargestThreeNum {
+	public static void main(String[] args) {
+        int x, y, z;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        x = sc.nextInt();
+        System.out.print("Enter the second number:");
+        y = sc.nextInt();
+        System.out.print("Enter the third number:");
+        z = sc.nextInt();
+        if(x > y && x > z)
+        {
+            System.out.println("Largest number is:"+x);
+        }
+        else if(y > z)
+        {
+            System.out.println("Largest number is:"+y);
+        }
+        else
+        {
+            System.out.println("Largest number is:"+z);
+        }
+        sc.close();
+
+	}
+
+}
+
+
+
